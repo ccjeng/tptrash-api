14967ce772a1068fa48368643169a51b6be3b7d1
Andy Cheng <ccjeng@gmail.com>
2014-05-02 06:45:42 -0400

    add cross domain

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ trashSchema.index({
 
 var Trash = mongoose.model('Trash', trashSchema, 'Trash');
 
+app.use(allowCrossDomain);
 app.get('/:hour/:num/:lng/:lat', function (req, res) {
     res.type('application/json');
     
@@ -63,6 +64,7 @@ app.get('/:hour/:num/:lng/:lat', function (req, res) {
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080  
  , ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+
 
 app.listen(port, ip);
 
